9f4a495a350c3b882ba500f0fe0d6b8c38043a65
Zechariah Guadiana <zechariah.guadiana@gmail.com>
2022-03-03 18:47:24 -0600

    initial commit for area of a circle true coders program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AreaOfCircle; 
+
+internal class AreaOfCircle
+{
+    public static double FindAreaOfCircle(double radius) {
+        Console.WriteLine("hello");
+        return Math.PI * radius * radius;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is the area of your circle?");
+        var radius = double.Parse(Console.ReadLine());
+        Console.WriteLine($"The area of a circle with radius {radius} is {FindAreaOfCircle(radius)}");
+    }
+}
